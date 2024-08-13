cfd3d91ca8fe5ae7a8dd9da05269637d72d70fb1
mdArsahd10 <mdarshd246@gmail.com>
2024-08-13 18:55:57 +0530

    added the navbar

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,9 +1,38 @@
-
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="bg-white ">
+      <div className="flex flex-row justify-between items-center mx-auto max-w-7xl h-16">
+        <div>
+          <h1 className="text-2xl font-bold">
+            Job <span className="text-[#F83002]">Portal</span>
+          </h1>
+        </div>
+        <div className="flex flex-row gap-5 items-center">
+          <ul className="flex font-medium items-center gap-5">
+            <li>Home</li>
+            <li>Jobs</li>
+            <li>Browse</li>
+          </ul>
+
+          <Popover>
+            <PopoverTrigger asChild>
+              <Avatar>
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="@shadcn"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </PopoverTrigger>
+            <PopoverContent></PopoverContent>
+          </Popover>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
